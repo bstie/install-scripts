6457010fdc1c9c04ec08250713f1d8d931c384db
bstie <benjaminstiegel@gmail.com>
2021-02-26 09:05:16 +0100

    Add install script for fail2ban

---
+++ fail2ban/README.md
@@ -0,0 +1,8 @@
+
+
+## Install
+
+This installs fail2ban, deploys a custom jail.local file and restarts fail2ban
+```bash
+curl -s "https://raw.githubusercontent.com/bstie/install-scripts/master/fail2ban/install.sh" | bash
+```
---
+++ fail2ban/install.sh
@@ -0,0 +1,5 @@
+# see also https://www.thomas-krenn.com/de/wiki/SSH_Login_unter_Debian_mit_fail2ban_absichern
+
+sudo apt update && sudo apt install fail2ban && \
+sudo wget https://raw.githubusercontent.com/bstie/install-scripts/master/fail2ban/jail.local -P /etc/fail2ban/ && \
+sudo systemctl restart fail2ban.service
