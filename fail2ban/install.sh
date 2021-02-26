f024168b51d50e6e75d3893d42075933a7e86cf9
bstie <benjaminstiegel@gmail.com>
2021-02-26 09:22:08 +0100

    Add echo after fail2ban

--- fail2ban/install.sh
+++ fail2ban/install.sh
@@ -3,4 +3,5 @@
 sudo apt update && sudo apt install fail2ban && \
 sudo wget https://raw.githubusercontent.com/bstie/install-scripts/master/fail2ban/jail.local -P /etc/fail2ban/ && \
 sudo systemctl restart fail2ban.service && \
-sudo systemctl status fail2ban.service
+sudo systemctl status fail2ban.service && \
+echo Finished - fail2ban is running
