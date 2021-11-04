b06c848bd74a102652cf466859aaf2dc1bcca5c3
bstie <BenjaminStiegel@gmail.com>
2021-11-04 15:07:20 +0100

    And again

--- neofetch/install.sh
+++ neofetch/install.sh
@@ -4,6 +4,7 @@
 set -e
 
 sudo apt update && sudo apt -y install wget neofetch && \
+neofetch && \
 wget -O - https://raw.githubusercontent.com/bstie/install-scripts/master/neofetch/config > ~/.config/neofetch/config && \
 echo -e '\nneofetch' >> ~/.bashrc && \
 echo "neofetch installed and added to .bashrc"
